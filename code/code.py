68e316b042e27ecf0d7f69d0e27f2bcfdbbff49f
HarshitPnt <102605899+HarshitPnt@users.noreply.github.com>
2022-04-02 23:04:23 +0530

    Add files via upload

---
+++ code/code.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+for i in range(1,20):
+    	
+	#input parameter
+    
+	x=i*0.3
+        y = (1+1/np.tan(x)-1/np.sin(x))*(1+np.tan(x)+1/np.cos(x))
+        
+       #output
+       
+	print(y)
