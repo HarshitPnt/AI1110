81e320c7598fb648930a8afef151947e0e9748c8
HarshitPnt <102605899+HarshitPnt@users.noreply.github.com>
2022-07-12 08:29:39 +0530

    Add files via upload

--- Random_Variables_Assignment/code/code7_3.py
+++ Random_Variables_Assignment/code/code7_3.py
@@ -3,15 +3,15 @@ import mpmath as mp
 import matplotlib.pyplot as plt
 import math
 
-def proberr2(x):
+def proberr_the_gen(x):
 	return 1/2*(1-math.sqrt(x/(x+2)))
 
-expected_err_vec = np.vectorize(proberr2, otypes=[np.float64])
+proberr_the = np.vectorize(proberr_the_gen, otypes=[np.float64])
 y=np.loadtxt('err.dat',dtype="double")
 x=np.linspace(1,10,10)
 plt.semilogy(x,y,marker='o',color="red",label="Simulated")
 x = np.linspace(1,10,1000)
-plt.semilogy(x, expected_err_vec(x),color="blue",label="Theoretical")
+plt.semilogy(x, proberr_the(x),color="blue",label="Theoretical")
 plt.grid()
 plt.xlabel("$\gamma$")
 plt.ylabel("$P_e(\gamma)$")
