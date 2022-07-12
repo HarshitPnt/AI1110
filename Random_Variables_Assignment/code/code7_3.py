b18860ed15244d399d62aec4de819d26e7561940
HarshitPnt <102605899+HarshitPnt@users.noreply.github.com>
2022-07-12 08:26:37 +0530

    Update code7_3.py

--- Random_Variables_Assignment/code/code7_3.py
+++ Random_Variables_Assignment/code/code7_3.py
@@ -3,22 +3,6 @@ import mpmath as mp
 import matplotlib.pyplot as plt
 import math
 
-
-# def cdf_the_gen(x):
-#     return (1-mp.exp(-x**2/2))
-
-# cdf_sim=[]
-# y=np.loadtxt('ray.dat',dtype="double")
-# x=np.linspace(0,10,100)
-
-# for i in range(0,100):
-#     cdf_sim.append(np.size(np.nonzero(y<x[i]))/1000000)
-
-# cdf_the=np.vectorize(cdf_the_gen,otypes=[np.float64])
-
-# plt.plot(x,cdf_the(x),color="blue")
-# plt.scatter(x,cdf_sim,color="orange")
-
 def proberr2(x):
 	return 1/2*(1-math.sqrt(x/(x+2)))
 
